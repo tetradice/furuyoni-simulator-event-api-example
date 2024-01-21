4bf9dddbe74ea056822cba34ad253b5c63b88767
Dice <tetradice@gmail.com>
2024-01-21 23:40:42 +0900

    3つの拡張イベントに対応

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,19 @@
+import "@tetradice/furuyoni-simulator-event-api-types";
+
+document.addEventListener("sakuraba.megamiSelectFinished", (e) => {
+  console.warn(
+    `sakuraba.megamiSelectFinished イベントが呼び出されました。`,
+    e.detail
+  );
+});
+
 document.addEventListener("sakuraba.reshuffled", (e) => {
-  console.warn(`sakuraba.reshuffled イベントが呼び出されました。`, e);
+  console.warn(`sakuraba.reshuffled イベントが呼び出されました。`, e.detail);
+});
+
+document.addEventListener("sakuraba.specialCardOpened", (e) => {
+  console.warn(
+    `sakuraba.specialCardOpened イベントが呼び出されました。`,
+    e.detail
+  );
 });
