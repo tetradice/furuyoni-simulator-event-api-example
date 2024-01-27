bb4fd9563e02ccf65e1aa0f1893e6a531517d072
Dice <tetradice@gmail.com>
2024-01-27 19:38:58 +0900

    対象イベント追加

--- src/content.ts
+++ src/content.ts
@@ -1,8 +1,36 @@
 /// <reference types="@tetradice/furuyoni-simulator-event-api-types" />
 
+document.addEventListener("sakuraba.megamiSelected", (e) => {
+  console.warn(
+    `sakuraba.megamiSelected イベントが呼び出されました。`,
+    e.detail
+  );
+});
+
 document.addEventListener("sakuraba.megamiSelectFinished", (e) => {
   console.warn(
     `sakuraba.megamiSelectFinished イベントが呼び出されました。`,
+    e.detail
+  );
+});
+
+document.addEventListener("sakuraba.removingMegamiSelected", (e) => {
+  console.warn(
+    `sakuraba.removingMegamiSelected イベントが呼び出されました。`,
+    e.detail
+  );
+});
+
+document.addEventListener("sakuraba.removingMegamiSelectFinished", (e) => {
+  console.warn(
+    `sakuraba.removingMegamiSelectFinished イベントが呼び出されました。`,
+    e.detail
+  );
+});
+
+document.addEventListener("sakuraba.turnPlayerDecided", (e) => {
+  console.warn(
+    `sakuraba.turnPlayerDecided イベントが呼び出されました。`,
     e.detail
   );
 });
